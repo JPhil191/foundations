59281e9898eff5fcf9dd2ae7ccb70bef6cd0ca40
Jan-Philipp Thiele <j-phil.thiele@hotmai.de>
2019-04-11 14:12:01 +0200

    message

--- project/news_website/empty_database.py
+++ project/news_website/empty_database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-db_connection = sqlite3.connect('/Users/jan-philippthiele/FoundationsFolder/foundations/project/instance/news_articles.sqlite')
+db_connection = sqlite3.connect('/home/janphilipp_thiele/foundations/project/instance/news_articles.sqlite')
 db_cursor = db_connection.cursor()
 db_cursor.execute("DELETE FROM Links;")
 
