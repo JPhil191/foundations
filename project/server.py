3c954cf720f219f422e552bffc51c048e8cefeb1
Jan-Philipp Thiele <j-phil.thiele@hotmai.de>
2019-04-11 13:07:53 +0200

    message

--- project/server.py
+++ project/server.py
@@ -1,4 +1,4 @@
 from waitress import serve
-import irtp.__init__
+import news_website.__init__
 
-serve(irtp.__init__.app, host='0.0.0.0', port=80)
+serve(news_website.__init__.app, host='0.0.0.0', port=80)
